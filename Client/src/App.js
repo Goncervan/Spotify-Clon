1e38d770d087e0018ce1adbbde1103273c74bfee
Goncervan <gonfedecer@gmail.com>
2022-03-17 15:59:20 -0300

    Deploy

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,12 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './Components/Login';
 import Dashboard from './Components/Dashboard';
-
+import { HashRouter, Routes, Route } from 'react-router-dom';
 const code = new URLSearchParams(window.location.search).get('code')
 
 
 function App() {
-  return code ? <Dashboard code={code} /> : <Login />
+  return (
+    <HashRouter>
+      <Routes>
+        <Route excact path='/Spotify-Clon/' element={<Login />} />
+        <Route path='/dashboard' element={<Dashboard code={code} />} />
+      </Routes>
+    </HashRouter>
+  )
 }
 
 export default App;
